64e81bec771e195c3b2728f0f288d7a1033e5e34
Zeeshan <zeeshan0703@outlook.com>
2022-10-05 14:17:34 +0530

    Master Added user input and allowed user control to print the number accordingly

--- text.py
+++ text.py
@@ -1,3 +1,9 @@
-print (this pr is made for educational purposes)
-for i in range(0,5)
- print (i)
+# This programme will start and end counting as per user requirement
+print("this pr is made for educational purposes")
+
+j=int(input("Enter a number from which you want to start counting:\t"))
+i=int(input("Enter a number from which you want to end counting:\t"))
+
+while(j<=i):
+    print(j)
+    j=j+1
